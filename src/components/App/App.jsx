0d4f005f528b5280cc1c2d938c46e1aedd50f086
mtsferreirasilva <mtsferreirasilva@gmail.com>
2018-06-03 18:37:32 -0400

    Adding album lazy loading feature

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,43 +4,75 @@ import { ImageList, HomeHeader } from '../';
 import './App.scss';
 
 class App extends Component {
+  state = {};
+  mounted = true;
+
+  componentDidMount() {
+    this.loadAlbums();
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  async loadAlbums() {
+    const undercityAlbum = await import(/* webpackChunkName: 'undercityAlbum' */ './albums/undercity');
+
+    if (!this.mounted) {
+      return;
+    }
+
+    this.setState({
+      undercityAlbum,
+    });
+  }
+
   render() {
-    return (
-      <main className="Page">
-        <div className="Layout">
-          <div className="LayoutItem LayoutItem--fill LayoutItem--center">
-            <HomeHeader />
+    const { undercityAlbum } = this.state;
+
+    if (!undercityAlbum) {
+      return <h1>Loading albums</h1>;
+    } else {
+      return (
+        <main className="Page">
+          <div className="Layout">
+            <div className="LayoutItem LayoutItem--fill LayoutItem--center">
+              <HomeHeader />
+            </div>
+            <div className="LayoutItem">
+              <ImageList
+                images={[
+                  {
+                    url: undercityAlbum.thumbnail.src,
+                    title: 'Undercity',
+                  },
+                  {
+                    url: 'https://placeimg.com/640/480/tech',
+                    title: 'Urban exploration',
+                  },
+                  {
+                    url: 'https://placeimg.com/640/480/arch',
+                    title: 'Canada & PNW ‘17',
+                  },
+                  {
+                    url: 'https://placeimg.com/640/480/people',
+                    title: 'Mt. Shasta ‘18',
+                  },
+                  {
+                    url: 'https://placeimg.com/640/480/nature',
+                    title: '春休み',
+                  },
+                  {
+                    url: 'https://placeimg.com/640/480/grayscale',
+                    title: 'Miscellaneous',
+                  },
+                ]}
+              />
+            </div>
           </div>
-          <div className="LayoutItem">
-            <ImageList
-              images={[
-                {
-                  url: 'https://placeimg.com/640/480/animals',
-                  title: 'Undercity',
-                },
-                {
-                  url: 'https://placeimg.com/640/480/tech',
-                  title: 'Urban exploration',
-                },
-                {
-                  url: 'https://placeimg.com/640/480/arch',
-                  title: 'Canada & PNW ‘17',
-                },
-                {
-                  url: 'https://placeimg.com/640/480/people',
-                  title: 'Mt. Shasta ‘18',
-                },
-                { url: 'https://placeimg.com/640/480/nature', title: '春休み' },
-                {
-                  url: 'https://placeimg.com/640/480/grayscale',
-                  title: 'Miscellaneous',
-                },
-              ]}
-            />
-          </div>
-        </div>
-      </main>
-    );
+        </main>
+      );
+    }
   }
 }
 
---
+++ src/components/App/albums/undercity/index.js
@@ -0,0 +1,13 @@
+import thumb from './thumb.jpg';
+import img1 from './1.jpg';
+
+const thumbnail = {
+  src: thumb,
+  label: 'Label',
+};
+const image1 = {
+  src: img1,
+  label: 'Label 2',
+};
+
+export { thumbnail, image1 };
