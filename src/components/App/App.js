acdd557e90c23ae10d3963c8874319d60bb3cbe1
mtsferreirasilva <mtsferreirasilva@gmail.com>
2018-06-03 01:38:13 -0400

    First commit

---
+++ src/components/App/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="Page">
+        <div className="Layout">
+          <div className="LayoutItem LayoutItem--fill LayoutItem--center">
+            <header>
+              <h1 className="Title">
+                <span role="img" aria-label="Wave emoji">👋</span> I'm Kai
+              </h1>
+            </header>
+            <div className="Content">
+              <p className="Quote">
+                I’m a designer and photographer born in Oakland, raised in Berkeley, commenced in San José, and living in<br/>San Francisco, California
+              </p>
+              <ul className="SocialLinks">
+                <li>
+                  <a href="https://www.instagram.com/kaigenji/" target="_blank" rel="noopener noreferrer">@kaigenji</a>
+                </li>
+                <li>
+                  <a href="mailto:kaigenji@gmail.com">email</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="LayoutItem">
+            SLIDER
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
