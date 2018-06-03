5401db4d65a56d7e8668a5cb5837bc7d4ee4cbd1
mtsferreirasilva <mtsferreirasilva@gmail.com>
2018-06-03 16:49:52 -0400

    HomeHeader tests

--- src/components/HomeHeader/HomeHeader.test.js
+++ src/components/HomeHeader/HomeHeader.test.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from 'enzyme';
 
 import HomeHeader from './HomeHeader';
 
 describe('HomeHeader', () => {
-  it('renders quote', () => {
-    const homeHeader = shallow(<HomeHeader />);
-    expect(homeHeader.find('div.HomeHeader__Quote')).toBeTruthy();
+  it('renders Title', () => {
+    const homeHeader = render(<HomeHeader />);
+    expect(homeHeader.find('.HomeHeader__Title').text()).toContain("I'm Kai");
+  });
+
+  it('renders Quote', () => {
+    const homeHeader = render(<HomeHeader />);
+    expect(homeHeader.find('.HomeHeader__Quote').text()).toContain(
+      'I’m a designer and photographer born in Oakland, raised in Berkeley, commenced in San José, and living inSan Francisco, California'
+    );
+  });
+
+  it('renders Instagram link', () => {
+    const homeHeader = render(<HomeHeader />);
+    const link = homeHeader.find('.HomeHeader__SocialLinks a', {
+      href: 'https://www.instagram.com/kaigenji/',
+    });
+    expect(link).toBeTruthy();
+    expect(link.text()).toContain('@kaigenji');
+  });
+
+  it('renders Email link', () => {
+    const homeHeader = render(<HomeHeader />);
+    const link = homeHeader.find('.HomeHeader__SocialLinks a', {
+      href: 'mailto:kaigenji@gmail.com',
+    });
+    expect(link).toBeTruthy();
+    expect(link.text()).toContain('email');
   });
 });
